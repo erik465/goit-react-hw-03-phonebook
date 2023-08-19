d0276215e694b1e07cac2a2e36cfda4c155ac07c
erik465 <ehalasz06@gmail.com>
2023-08-19 19:32:56 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ import { ContactFilter } from "./ContactFilter/ContactFilter";
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-11'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter : '',
     
   }
@@ -58,6 +53,23 @@ export class App extends Component {
         filter : filterQuery
       }
     })
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+      if(prevState.contacts != this.state.contacts){
+          localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+      }
+  }
+
+  componentDidMount(){
+    if(localStorage.getItem('contacts')){
+      this.setState(prevState => {
+        return {
+            ...prevState,
+            contacts: JSON.parse(localStorage.getItem('contacts'))
+          }
+      })
+    }
   }
 
 
